f2563b6f9ca771fddccc3979ccb5272223b434b0
Admin <admin@MacBook-Pro-Admin.local>
2023-07-18 15:57:01 +0600

    fix str models email

--- apps/users/models.py
+++ apps/users/models.py
@@ -32,7 +32,7 @@ class User(AbstractUser):
         verbose_name_plural = "Пользователи"
 
     def __str__(self):
-        return self.uniqueId.__str__()
+        return self.email.__str__()
 
     username = None
     date_joined = None
